c29cc6115da2cdd31c8316e1476378cd2b111bae
Behdad Esfahbod <behdad@gnome.org>
2007-08-07 19:00:45 +0000

    Bug 464410 – license confusion
    2007-08-07  Behdad Esfahbod  <behdad@gnome.org>
    
            Bug 464410 – license confusion
    
            "gnome-terminal states that it is GPL, and COPYING is the GPL,
            but every single source file seems to have a LGPL header."
    
            * src/eggaccelerators.c:
            * src/eggcellrendererkeys.c:
            * src/encoding.c:
            * src/profile-editor.c:
            * src/simple-x-font-selector.c:
            * src/skey-popup.c:
            * src/terminal-accels.c:
            * src/terminal-notebook.c:
            * src/terminal-profile.c:
            * src/terminal-screen.c:
            * src/terminal-widget-vte.c:
            * src/terminal-window.c:
            * src/terminal.c:
            Replace LPGL header with GPL header.
            Note that LPGL allows conversion to GPL, so this change is legal
            anyway.
    
    
    svn path=/trunk/; revision=2242

--- src/terminal-notebook.c
+++ src/terminal-notebook.c
@@ -1,22 +1,23 @@
 /*
- *  Copyright (C) 2002 Christophe Fergeau
- *  Copyright (C) 2003, 2004 Marco Pesenti Gritti
- *  Copyright (C) 2003, 2004 Christian Persch
- *  Copyright (C) 2005 Tony Tsui
+ * Copyright (C) 2002 Christophe Fergeau
+ * Copyright (C) 2003, 2004 Marco Pesenti Gritti
+ * Copyright (C) 2003, 2004 Christian Persch
+ * Copyright (C) 2005 Tony Tsui
  *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2, or (at your option)
- *  any later version.
+ * This file is part of gnome-terminal.
  *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
+ * Gnome-terminal is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
  *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ * Gnome-terminal is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include "terminal-notebook.h"
