e393425a9444f559c6b380f749dc515e72f31730
Christian Persch <chpe@src.gnome.org>
2008-05-29 19:59:00 +0000

    (C) update.
    svn path=/trunk/; revision=2838

--- src/terminal-app.h
+++ src/terminal-app.h
@@ -1,5 +1,6 @@
 /*
  * Copyright © 2001 Havoc Pennington
+ * Copyright © 2008 Christian Persch
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
