2829430edbda01abafabb8a6b9610471d9372679
Christian Persch <chpe@src.gnome.org>
2008-06-15 20:44:04 +0000

    Rename.
    svn path=/trunk/; revision=2889

--- autogen.sh
+++ autogen.sh
@@ -9,14 +9,6 @@ REQUIRED_INTLTOOL_VERSION=0.40.0
 
 PKG_NAME="gnome-terminal"
 
-(test -f $srcdir/configure.in \
-  && test -f $srcdir/README \
-  && test -d $srcdir/src) || {
-    echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
-    echo " top-level $PKG_NAME directory"
-    exit 1
-}
-
 which gnome-autogen.sh || {
     echo "You need to install gnome-common from the GNOME SVN"
     exit 1
