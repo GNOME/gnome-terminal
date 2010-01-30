ea76ff9cefbc178b11020e476e37b18349e4b3f9
Behdad Esfahbod <behdad@behdad.org>
2010-01-29 19:30:45 -0500

    Remove stale comments

--- src/skey/sha1.c
+++ src/skey/sha1.c
@@ -1,17 +1,3 @@
-/*
- * SHA-1 in C
- * By Steve Reid <steve@edmweb.com>
- * 100% Public Domain
- *
- * Test Vectors (from FIPS PUB 180-1)
- * "abc"
- *   A9993E36 4706816A BA3E2571 7850C26C 9CD0D89D
- * "abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq"
- *   84983E44 1C3BD26E BAAE4AA1 F95129E5 E54670F1
- * A million repetitions of "a"
- *   34AA973C D4C4DAA4 F61EEB2B DBAD2731 6534016F
- */
-
 #include <config.h>
 
 #include <string.h>
