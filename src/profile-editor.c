e6dc7a41a49ab4a3a95d9bfa6c7649b6c655a90a
Christian Persch <chpe@gnome.org>
2010-10-28 22:33:28 +0200

    Fix the gtk3 build for combo box text API changes
    Bug #633226.

--- src/profile-editor.c
+++ src/profile-editor.c
@@ -509,16 +509,28 @@ reset_compat_defaults_cb (GtkWidget       *button,
  */
 
 static void
-init_color_scheme_menu (GtkWidget *combo_box)
+init_color_scheme_menu (GtkWidget *widget)
 {
+  GtkCellRenderer *renderer;
+  GtkTreeIter iter;
+  GtkListStore *store;
   int i;
 
-  i = G_N_ELEMENTS (color_schemes);
-  while (i > 0)
-    {
-      gtk_combo_box_prepend_text (GTK_COMBO_BOX (combo_box),
-                                  _(color_schemes[--i].name));
-    }
+  store = gtk_list_store_new (1, G_TYPE_STRING);
+  for (i = 0; i < G_N_ELEMENTS (color_schemes); ++i)
+    gtk_list_store_insert_with_values (store, &iter, -1,
+                                       0, _(color_schemes[i].name),
+                                       -1);
+  gtk_list_store_insert_with_values (store, &iter, -1,
+                                      0, _("Custom"),
+                                      -1);
+
+  gtk_combo_box_set_model (GTK_COMBO_BOX (widget), GTK_TREE_MODEL (store));
+  g_object_unref (store);
+
+  renderer = gtk_cell_renderer_text_new ();
+  gtk_cell_layout_pack_start (GTK_CELL_LAYOUT (widget), renderer, TRUE);
+  gtk_cell_layout_set_attributes (GTK_CELL_LAYOUT (widget), renderer, "text", 0, NULL);
 }
 
 static char*
