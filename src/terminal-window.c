35a509165750d0ecefe9f26702d0f3c0e230f0c2
Christian Persch <chpe@gnome.org>
2012-03-14 22:00:01 +0100

    window: Use correct scroll direction
    Fix thinko in last commit.

--- src/terminal-window.c
+++ src/terminal-window.c
@@ -2775,16 +2775,16 @@ notebook_scroll_event_cb (GtkWidget      *widget,
       switch (gtk_notebook_get_tab_pos (notebook)) {
         case GTK_POS_LEFT:
         case GTK_POS_RIGHT:
-          if (event->delta_x > 0)
+          if (event->delta_y > 0)
             gtk_notebook_next_page (notebook);
-          else if (event->delta_x < 0)
+          else if (event->delta_y < 0)
             gtk_notebook_prev_page (notebook);
           break;
         case GTK_POS_TOP:
         case GTK_POS_BOTTOM:
-          if (event->delta_y > 0)
+          if (event->delta_x > 0)
             gtk_notebook_next_page (notebook);
-          else if (event->delta_y < 0)
+          else if (event->delta_x < 0)
             gtk_notebook_prev_page (notebook);
           break;
       }
