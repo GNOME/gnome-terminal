71611a40e7bb762e3d24dbf3947f0ad33f5cfe14
Christian Persch <chpe@gnome.org>
2013-01-22 14:56:59 +0100

    profile: editor: Make bold colourpicker insensitive when same-colour chosen
    When the 'same colour as text' checkbox is active, make the colourpicker
    insensitive.

--- src/profile-editor.c
+++ src/profile-editor.c
@@ -849,6 +849,13 @@ terminal_profile_edit (GSettings  *profile,
                    gtk_builder_get_object (builder,
                                            "bold-color-same-as-fg-checkbox"),
                    "active", G_SETTINGS_BIND_GET | G_SETTINGS_BIND_SET);
+  g_settings_bind (profile, TERMINAL_PROFILE_BOLD_COLOR_SAME_AS_FG_KEY,
+                   gtk_builder_get_object (builder,
+                                           "bold-colorpicker-box"),
+                   "sensitive", 
+                   G_SETTINGS_BIND_GET |
+                   G_SETTINGS_BIND_INVERT_BOOLEAN |
+                   G_SETTINGS_BIND_NO_SENSITIVITY);
   g_settings_bind_with_mapping (profile, TERMINAL_PROFILE_BOLD_COLOR_KEY,
                                 gtk_builder_get_object (builder,
                                                         "bold-colorpicker"),
