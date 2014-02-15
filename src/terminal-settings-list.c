55cbe7df9420bc9082bfed94a61931957ea0c440
Christian Persch <chpe@gnome.org>
2014-02-15 15:42:02 +0100

    profile: Fix dummy settings patch
    Fixes crash when cloning a profile.
    
    https://retrace.fedoraproject.org/faf/problems/1433130/
    https://bugzilla.gnome.org/show_bug.cgi?id=724264
    (cherry picked from commit 512e4bbf467d1eab91bba86dcd27f8852fc00273)
    
    Conflicts:
    	src/terminal-settings-list.c
    
    (cherry picked from commit 3de426475c0096c9ef4ea1806fb9380db3914603)

--- src/terminal-settings-list.c
+++ src/terminal-settings-list.c
@@ -289,7 +289,7 @@ clone_child (TerminalSettingsList *list,
 
   /* FIXME: this is beyond ugly. Need API on GSettingsSchema to list all the keys! */
   {
-    GSettings *dummy = g_settings_new_with_path (list->child_schema_id, "/foo");
+    GSettings *dummy = g_settings_new_with_path (list->child_schema_id, "/foo/");
     keys = g_settings_list_keys (dummy);
     g_object_unref (dummy);
   }
