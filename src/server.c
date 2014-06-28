de52dfc064eccf5b88bdaa76d5e16b54711a408b
Christian Persch <chpe@gnome.org>
2014-06-28 11:18:17 +0200

    server: Improve error message
    Inform about the charset when non-UTF-8.
    
    (cherry picked from commit d74a7267dd9ad1b61da2ce7182584afe381009a1)

--- src/server.c
+++ src/server.c
@@ -74,7 +74,7 @@ main (int argc, char **argv)
 {
   GApplication *app;
   int exit_code = EXIT_FAILURE;
-  const char *home_dir;
+  const char *home_dir, *charset;
   GError *error = NULL;
 
   if (G_UNLIKELY ((getuid () != geteuid () ||
@@ -89,8 +89,8 @@ main (int argc, char **argv)
 
   terminal_i18n_init (TRUE);
 
-  if (!g_get_charset (NULL)) {
-    g_printerr ("Non UTF-8 locale is not supported!\n");
+  if (!g_get_charset (&charset)) {
+    g_printerr ("Non UTF-8 locale (%s) is not supported!\n", charset);
     return _EXIT_FAILURE_NO_UTF8;
   }
 
