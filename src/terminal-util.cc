48936582ddce1d1f23c0c42565887f63cd589187
Christian Persch <chpe@src.gnome.org>
2021-06-30 18:00:45 +0200

    settings: Fix crash when loading the reference schema source fails
    https://retrace.fedoraproject.org/faf/reports/190312/

--- src/terminal-util.cc
+++ src/terminal-util.cc
@@ -1926,7 +1926,6 @@ terminal_g_settings_schema_source_get_default(void)
                "Using unverified installed schemas.\n",
                error->message);
 
-    g_settings_schema_source_unref(reference_source);
     return g_settings_schema_source_ref(default_source);
   }
 
