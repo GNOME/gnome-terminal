0dd30128e6f0b23db0f1f2a69d745fcef0d8758e
Christian Persch <chpe@src.gnome.org>
2022-11-19 21:59:43 +0100

    screen: Add some missing casts

--- src/terminal-screen.cc
+++ src/terminal-screen.cc
@@ -2268,7 +2268,7 @@ terminal_screen_has_foreground_process (TerminalScreen *screen,
   if (sysctl (mib, G_N_ELEMENTS (mib), nullptr, &len, nullptr, 0) == -1)
       return TRUE;
 
-  data_buf = g_malloc0 (len);
+  data_buf = (char*)g_malloc0 (len);
   if (sysctl (mib, G_N_ELEMENTS (mib), data_buf, &len, nullptr, 0) == -1)
       return TRUE;
   data = data_buf;
@@ -2280,7 +2280,7 @@ terminal_screen_has_foreground_process (TerminalScreen *screen,
   if (sysctl (mib, G_N_ELEMENTS (mib), nullptr, &len, nullptr, 0) == -1)
       return TRUE;
 
-  data_buf = g_malloc0 (len);
+  data_buf = (char*)g_malloc0 (len);
   if (sysctl (mib, G_N_ELEMENTS (mib), data_buf, &len, nullptr, 0) == -1)
       return TRUE;
   data = ((char**)data_buf)[0];
