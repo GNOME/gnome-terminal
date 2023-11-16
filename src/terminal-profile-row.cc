d66bc220286c0ef97420b4237f13c3e667175099
Christian Persch <chpe@src.gnome.org>
2023-11-16 21:53:34 +0100

    all: Remove unwanted data

--- src/terminal-profile-row.cc
+++ src/terminal-profile-row.cc
@@ -13,8 +13,6 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
 #include "config.h"
