GNOME/gnome-terminal

commit 0f9a83fa4cddaf703662d0eecad1c086b12f647a

window: Fix inserting new tabs
... when the parent tab is pinned and not the last pinned one.

Fixes: https://gitlab.gnome.org/GNOME/gnome-terminal/-/issues/8025

src/terminal-window.cc
@@ -303,6 +303,7 @@ action_new_terminal_cb (GSimpleAction *action,
   }
 
   TerminalScreen *parent_screen = window->active_screen;
+  auto const parent_tab = parent_screen ? terminal_tab_get_from_screen(parent_screen) : nullptr;
 
   profiles_list = terminal_app_get_profiles_list (app);
   if (g_str_equal (uuid_str, "current"))
@@ -321,9 +322,10 @@ action_new_terminal_cb (GSimpleAction *action,
   TerminalScreen *screen = terminal_screen_new (profile,
                                                 nullptr /* title */,
                                                 1.0);
+  auto const tab = terminal_tab_new(screen);
 
   /* Now add the new screen to the window */
-  terminal_window_add_screen (window, screen, -1);
+  terminal_window_add_tab(window, tab, parent_tab);
   terminal_window_switch_screen (window, screen);
   gtk_widget_grab_focus (GTK_WIDGET (screen));
 
@@ -2406,41 +2408,22 @@ terminal_window_get_active_screen_num (TerminalWindow *window)
 }
 
 void
-terminal_window_add_screen (TerminalWindow *window,
-                            TerminalScreen *screen,
-                            int            position)
+terminal_window_add_tab(TerminalWindow* window,
+                        TerminalTab* tab,
+                        TerminalTab* parent_tab)
 {
-  GtkWidget *old_window;
+  auto const global_settings = terminal_app_get_global_settings(terminal_app_get());
+  auto const position_pref = TerminalNewTabPosition
+    (g_settings_get_enum(global_settings, TERMINAL_SETTING_NEW_TAB_POSITION_KEY));
 
-  old_window = GTK_WIDGET (gtk_widget_get_root (GTK_WIDGET (screen)));
-  if (GTK_IS_ROOT (old_window) &&
-      TERMINAL_IS_WINDOW (old_window) &&
-      TERMINAL_WINDOW (old_window)== window)
-    return;
-
-  if (TERMINAL_IS_WINDOW (old_window))
-    terminal_window_remove_screen (TERMINAL_WINDOW (old_window), screen);
-
-  if (position == -1) {
-    GSettings *global_settings = terminal_app_get_global_settings (terminal_app_get ());
-    TerminalNewTabPosition position_pref = TerminalNewTabPosition
-      (g_settings_get_enum (global_settings,
-			    TERMINAL_SETTING_NEW_TAB_POSITION_KEY));
-    switch (position_pref) {
-    case TERMINAL_NEW_TAB_POSITION_NEXT:
-      position = terminal_window_get_active_screen_num (window) + 1;
-      break;
-
-    default:
-    case TERMINAL_NEW_TAB_POSITION_LAST:
-      position = -1;
-      break;
-    }
+  if (position_pref == TERMINAL_NEW_TAB_POSITION_NEXT &&
+      parent_tab) {
+    terminal_notebook_add_tab(window->notebook, tab, parent_tab);
+  } else {
+    terminal_notebook_append_tab(window->notebook, tab, false);
   }
 
-  terminal_notebook_add_screen (window->notebook, screen, position);
-
-  gtk_widget_grab_focus (GTK_WIDGET (screen));
+  gtk_widget_grab_focus(GTK_WIDGET(terminal_tab_get_screen(tab)));
 }
 
 void

src/terminal-gdbus.cc
@@ -518,7 +518,9 @@ terminal_factory_impl_create_instance (TerminalFactory *factory,
 
   /* Now we can create the new screen */
   TerminalScreen *screen = terminal_screen_new (profile, title, zoom);
-  terminal_window_add_screen (window, screen, -1);
+  auto const tab = terminal_tab_new(screen);
+  auto const parent_tab = parent_screen ? terminal_tab_get_from_screen(parent_screen) : nullptr;
+  terminal_window_add_tab(window, tab, parent_tab);
 
   /* Apply window properties */
   gboolean active;

src/terminal-notebook.cc
@@ -89,26 +89,49 @@ remove_reorder_bindings (GtkWidgetClass *widget_class,
 }
 
 void
-terminal_notebook_add_screen (TerminalNotebook *notebook,
-                              TerminalScreen   *screen,
-                              int               position)
+terminal_notebook_add_tab(TerminalNotebook *notebook,
+                          TerminalTab* tab,
+                          TerminalTab* parent_tab)
 {
-  AdwTabPage *page;
+  g_return_if_fail(TERMINAL_IS_NOTEBOOK(notebook));
+  g_return_if_fail(TERMINAL_IS_TAB(tab));
+  g_return_if_fail(!parent_tab || TERMINAL_IS_TAB(parent_tab));
+  g_return_if_fail(!gtk_widget_get_parent(GTK_WIDGET(tab)));
 
-  g_return_if_fail (TERMINAL_IS_NOTEBOOK (notebook));
-  g_return_if_fail (TERMINAL_IS_SCREEN (screen));
-  g_return_if_fail (gtk_widget_get_parent (GTK_WIDGET (screen)) == nullptr);
+  if (!parent_tab)
+    return terminal_notebook_append_tab(notebook, tab, false);
 
-  if (position < 0)
-    position = adw_tab_view_get_n_pages (notebook->tab_view);
+  auto const parent_page = adw_tab_view_get_page(notebook->tab_view,
+                                                 GTK_WIDGET(parent_tab));
+  g_return_if_fail(parent_page);
 
-  page = adw_tab_view_insert (notebook->tab_view,
-                              terminal_tab_new (screen),
-                              position);
+  auto const page = adw_tab_view_add_page(notebook->tab_view,
+                                          GTK_WIDGET(tab),
+                                          parent_page);
 
-  g_object_bind_property (screen, "title",
-                          page, "title",
-                          G_BINDING_SYNC_CREATE);
+  auto const screen = terminal_tab_get_screen(tab);
+  g_object_bind_property(screen, "title",
+                         page, "title",
+                         G_BINDING_SYNC_CREATE);
+}
+
+void
+terminal_notebook_append_tab(TerminalNotebook *notebook,
+                             TerminalTab* tab,
+                             bool pinned)
+{
+  g_return_if_fail(TERMINAL_IS_NOTEBOOK(notebook));
+  g_return_if_fail(TERMINAL_IS_TAB(tab));
+  g_return_if_fail(!gtk_widget_get_parent(GTK_WIDGET(tab)));
+
+  auto const page = pinned ? adw_tab_view_append_pinned(notebook->tab_view,
+                                                        GTK_WIDGET(tab))
+    : adw_tab_view_append(notebook->tab_view, GTK_WIDGET(tab));
+
+  auto const screen = terminal_tab_get_screen(tab);
+  g_object_bind_property(screen, "title",
+                         page, "title",
+                         G_BINDING_SYNC_CREATE);
 }
 
 void

src/terminal-notebook.hh
@@ -27,9 +27,12 @@
 G_DECLARE_FINAL_TYPE (TerminalNotebook, terminal_notebook, TERMINAL, NOTEBOOK, GtkWidget)
 
 GtkWidget      *terminal_notebook_new                    (void);
-void            terminal_notebook_add_screen             (TerminalNotebook *notebook,
-                                                          TerminalScreen   *screen,
-                                                          int               position);
+void            terminal_notebook_add_tab(TerminalNotebook *notebook,
+                                          TerminalTab* tab,
+                                          TerminalTab* parent_tab);
+void            terminal_notebook_append_tab(TerminalNotebook *notebook,
+                                             TerminalTab* tab,
+                                             bool pinned);
 void            terminal_notebook_remove_screen          (TerminalNotebook *notebook,
                                                           TerminalScreen   *screen);
 TerminalScreen *terminal_notebook_get_active_screen      (TerminalNotebook *notebook);

src/terminal-tab.cc
@@ -190,10 +190,10 @@ terminal_tab_class_init (TerminalTabClass *klass)
  *
  * Returns: a new #TerminalTab for @screen
  */
-GtkWidget *
+TerminalTab*
 terminal_tab_new (TerminalScreen *screen)
 {
-  return reinterpret_cast<GtkWidget*>
+  return reinterpret_cast<TerminalTab*>
     (g_object_new (TERMINAL_TYPE_TAB,
 		   "screen", screen,
 		   nullptr));

src/terminal-tab.hh
@@ -28,7 +28,7 @@ G_BEGIN_DECLS
 
 G_DECLARE_FINAL_TYPE (TerminalTab, terminal_tab, TERMINAL, TAB, GtkWidget)
 
-GtkWidget *terminal_tab_new (TerminalScreen *screen);
+TerminalTab* terminal_tab_new (TerminalScreen *screen);
 
 TerminalScreen *terminal_tab_get_screen (TerminalTab *tab);
 

src/terminal-window.hh
@@ -21,6 +21,7 @@
 #include <adwaita.h>
 
 #include "terminal-screen.hh"
+#include "terminal-tab.hh"
 
 G_BEGIN_DECLS
 
@@ -32,9 +33,9 @@ TerminalWindow *terminal_window_new                      (GApplication   *app);
 int             terminal_window_get_active_screen_num    (TerminalWindow *window);
 void            terminal_window_set_titlebar             (TerminalWindow *window,
                                                           GtkWidget      *titlebar);
-void            terminal_window_add_screen               (TerminalWindow *window,
-                                                          TerminalScreen *screen,
-                                                          int             position);
+void            terminal_window_add_tab(TerminalWindow *window,
+                                        TerminalTab* tab,
+                                        TerminalTab* parent_tab);
 void            terminal_window_remove_screen            (TerminalWindow *window,
                                                           TerminalScreen *screen);
 void            terminal_window_update_size              (TerminalWindow *window);

commit b6c69a550430e640c8dba6524fc07df9fcb3b5c7

window: Fix creating new windows
Fixes a regression from 0f9a83fa4cddaf703662d0eecad1c086b12f647a.

When we open a new window, we must not use a parent tab for the newly
to-be-inserted tab since the parent tab belongs to a different window.

https://gitlab.gnome.org/GNOME/gnome-terminal/-/issues/8025

src/terminal-window.cc
@@ -303,7 +303,7 @@ action_new_terminal_cb (GSimpleAction *action,
   }
 
   TerminalScreen *parent_screen = window->active_screen;
-  auto const parent_tab = parent_screen ? terminal_tab_get_from_screen(parent_screen) : nullptr;
+  auto parent_tab = parent_screen ? terminal_tab_get_from_screen(parent_screen) : nullptr;
 
   profiles_list = terminal_app_get_profiles_list (app);
   if (g_str_equal (uuid_str, "current"))
@@ -316,15 +316,18 @@ action_new_terminal_cb (GSimpleAction *action,
   if (profile == nullptr)
     return;
 
-  if (mode == TERMINAL_NEW_TERMINAL_MODE_WINDOW)
+  if (mode == TERMINAL_NEW_TERMINAL_MODE_WINDOW) {
     window = terminal_window_new (G_APPLICATION (app));
+    // Cannot use not use the parent tab since it belongs to a different window
+    parent_tab = nullptr;
+  }
 
   TerminalScreen *screen = terminal_screen_new (profile,
                                                 nullptr /* title */,
                                                 1.0);
   auto const tab = terminal_tab_new(screen);
 
-  /* Now add the new screen to the window */
+  // Now add the new screen to the window.
   terminal_window_add_tab(window, tab, parent_tab);
   terminal_window_switch_screen (window, screen);
   gtk_widget_grab_focus (GTK_WIDGET (screen));